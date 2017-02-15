862a3b4593d13a42d67a52e84584bef57f514d67
Chan Jun Shern <chanjunshern@gmail.com>
2017-02-15 10:12:23 +0000

    Added pygame animation and JSON messaging

--- clientPublisher.py
+++ clientPublisher.py
@@ -1,12 +1,37 @@
 import paho.mqtt.client as mqtt
+import time 
+import ujson
+
+class MessageObject(object):
+	def __init__(self, doorState):
+		self.timeString = getTimeAsString()
+		self.doorState = doorState
+
+def getTimeAsString():
+	# Returns time as DD/MM/YYYY - HH:MM:SS 
+	t = time.localtime()
+	timeString = ("%02d/%02d/%04d - %02d:%02d:%02d"
+		%(t.tm_mday, t.tm_mon, t.tm_year, t.tm_hour, t.tm_min, t.tm_sec))
+	return timeString
+
+def sendOpen():
+	doorState = 1
+	msg = MessageObject(doorState)
+	client.publish("esys/FourMusketeers", ujson.dumps(msg))
+
+def sendClose():
+	doorState = 0
+	msg = MessageObject(doorState)
+	client.publish("esys/FourMusketeers", ujson.dumps(msg))
 
 # This is the Publisher
-
 mqtt_client = mqtt.Client("", True, None, mqtt.MQTTv31)
 client = mqtt.Client()
 
-broker_addr = '192.168.0.10'
-client.connect('localhost',1883,60)
+# broker_addr = '192.168.0.10'
+broker_addr = 'localhost'
+client.connect(broker_addr,1883,60)
 
-client.publish("topic/test", "Hello world!")
-client.disconnect()
+client.publish("esys/FourMusketeers", "OPEN")
+client.publish("esys/FourMusketeers", "CLOSE")
+# client.disconnect()
--- userInterface.py
+++ userInterface.py
@@ -1,29 +1,72 @@
 import paho.mqtt.client as mqtt
 import time
+import pygame
+import ujson
 
-# This is the Subscriber
+class MessageObject(object):
+    def __init__(self, doorState):
+        self.timeString = getTimeAsString()
+        self.doorState = doorState
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("esys/FourMusketeers")
 
 def on_message(client, userdata, msg):
+    global doorState
     message = (msg.payload.decode())
-    print(message)
-    if (message == "OPEN"):
+    msgObject = ujson.loads(message)
+    print(msgObject['timeString'])
+    if (msgObject['doorState'] == "OPEN"):
     	doorState = 1
-    elif (message == "CLOSE"):
+    elif (msgObject['doorState'] == "CLOSE"):
     	doorState = 0
 
-global doorState 
+    # Update graphics
+    updateGraphics()
+
+def updateGraphics():
+    bgColor = pygame.Color(10,10,10) 
+    screen.fill(bgColor)
+    drawFloorPlan()
+    drawDoorState(doorState)
+    drawMessageLog()
+    pygame.display.update()
+    return
+
+def drawFloorPlan():
+    return
+
+def drawDoorState(doorOpen):
+    print("Door state is %s" %(doorOpen))
+    if doorOpen:
+        pygame.draw.rect(screen,(200,200,200),[20,20,250,30])
+    else:
+        pygame.draw.rect(screen,(200,200,200),[20,20,30,250])
+    return
+
+def drawMessageLog():
+    return
+
+## Global variables
 doorState = 0
-# client = mqtt.Client()
+
+## Graphics / UI
+pygame.init()
+size = (500, 700)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("IoT Home Security")
+updateGraphics()
+
+## MQTT message subscriber
+client = mqtt.Client()
 client = mqtt.Client("", True, None, mqtt.MQTTv31)
 
 client.on_connect = on_connect
 client.on_message = on_message
 
-broker_addr = '192.168.0.10'
+# broker_addr = '192.168.0.10'
+broker_addr = 'localhost'
 client.connect(broker_addr,1883,60)
 
 client.loop_forever()
